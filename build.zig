038f339ea8aa18610b839bcd15362256d3bfeea7
Jari Vetoniemi <jari.vetoniemi@cloudef.pw>
2024-02-27 14:14:00 +0900

    depend_on in build.zig

--- build.zig
+++ build.zig
@@ -1,2 +1,16 @@
 const std = @import("std");
-pub fn build(_: *std.Build) void {}
+
+pub fn build(b: *std.Build) void {
+    add(b, "tar");
+    add(b, "tgz");
+    add(b, "tar_gz");
+    add(b, "txz");
+    add(b, "tar_xz");
+    add(b, "tzst");
+    add(b, "tar_zst");
+    add(b, "git");
+}
+
+fn add(b: *std.Build, name: []const u8) void {
+    b.default_step.dependOn(b.dependency(name, .{}).builder.default_step);
+}
